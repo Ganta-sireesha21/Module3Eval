ee47da500d86dfd32a0358cc6736fd005f745d0a
Ganta-sireesha21 <gantasireesha1998@gmail.com>
2026-01-03 20:50:47 +0530

    mini res

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,30 @@
-import Component1 from "./Component1";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import { AuthProvider } from "./contexts/AuthContext";
+import Login from "./pages/Login";
+import AdminDashboard from "./pages/AdminDashboard";
+import CustomeDashboar from "./pages/CustomerDashboard";
+import ProtectedRoute from "./components/ProtectedRoute";
+export default function App() {
+    return (
+        <AuthProvider>
+            <BrowserRouter>
+                <Routes>
+                    <Route path="/login" element={<Login />} />
+                    <Route path="/admin/dashboard" element={
+                        <ProtectedRoute role="admin">
+                            <AdminDashboard />
+                        </ProtectedRoute>
+                    } />
+                    <Route path="/customer/home" element={
+                        <ProtectedRoute role="customer">
+                            <CustomeDashboar />
+                        </ProtectedRoute>
+                    } />
+                    <Route path="*" element={<Navigate to="/login" />} />
+                </Routes>
+            </BrowserRouter>
+        </AuthProvider>
+    );
+}     
 
-function App() {
-  return (
-    <div>
-      <h2>Solving Props Drilling Using Context API</h2>
-      <Component1 />
-    </div>
-  );
-}
 
-export default App;
